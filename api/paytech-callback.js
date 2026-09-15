a3946b2ee26408f2f06a098b36a06c7dee299dc8
Mgloot <asgmali821@gmail.com>
2026-09-15 22:00:59 +0000

    Delete api/paytech-callback.js

--- api/paytech-callback.js
+++
@@ -1,3 +0,0 @@
-// Alias : certains tableaux de bord PayTech pointent l'IPN vers /api/paytech-callback.
-// On réutilise exactement le même traitement sécurisé que /api/paytech-ipn.
-module.exports = require('./paytech-ipn.js');
